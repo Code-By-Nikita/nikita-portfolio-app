41f8892c0881043175ae8f27fbf9c6055e00f7ed
Nikita Choudhary <nikita.choudhary3108@gmail.com>
2025-05-26 05:04:58 +0530

    build: update packages and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nikita-portfolio-app/',
   plugins: [react()],
 })
